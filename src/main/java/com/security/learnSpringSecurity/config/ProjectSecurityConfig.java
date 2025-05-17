a4493b27fac3c8c6a30fd6e38b23ec468502d924
Rakesh Nair <crucifyrakesh@gmail.com>
2025-05-17 16:26:26 +0530

    Add scripts sql, few dependencies and connect to mysql

--- src/main/java/com/security/learnSpringSecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/security/learnSpringSecurity/config/ProjectSecurityConfig.java
@@ -11,8 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -30,10 +33,10 @@ public class ProjectSecurityConfig  {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$AsO2XYAnOAUCyKxNWwLhqei6xedU/bifptJH1EFXrv/icf9j5XeWy").authorities("write").build();
-        return new InMemoryUserDetailsManager(user,admin);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        //UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
+        //UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$AsO2XYAnOAUCyKxNWwLhqei6xedU/bifptJH1EFXrv/icf9j5XeWy").authorities("write").build();
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
